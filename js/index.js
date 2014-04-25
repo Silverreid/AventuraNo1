3c77a3fad75ecf65d5e9cbb1b2f6a0504d94ec67
Silverreid <silverreid1@gmail.com>
2014-04-25 17:38:31 -0500

    Muchos cambios
    Empezando a escribir codigo de la primera parte de la aventura,
    subiendo mod.js para habilitar los nuevos tipos de input en demas
    exploradores,
    Empezando co la logica y los efectos en el index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){ //iniciando con JQUERY
-  
+  $('#validaMayoria').submit(function(event){
+    var edad = $('#edad').val();
+    console.log(edad);
+    if(edad >=18){
+      $('#mnsj1').text('Muy bien puedes pasar');
+    }else{
+      $('#mnsj1').text('lo siento NO puedes pasar');
+    };
+      $('#cadenero article:first-child').fadeOut('slow',function(){
+        $('#mnsj1').fadeIn();
+      });
+    event.preventDefault();
+  });
+
 });
