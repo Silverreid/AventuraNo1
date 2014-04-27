91f988bb36e039af08076141415f806e15a751f4
Silverreid <silverreid1@gmail.com>
2014-04-27 13:24:49 -0500

    Dando estructura y aumentado el contenido
    Aumentando el codigo en el index que se van a ocupar mas adelante, esto
    incluye los form, section articles
    
    Creacion de estilos generales qe se van a ocupar en diferente momentos
    asi como los estilos propios de cada seccion del html
    
    Codificación de la función para verificar la edad y mostrar resultado de
    acuerdo al rango de edad

--- js/index.js
+++ js/index.js
@@ -1,16 +1,21 @@
 $(document).ready(function(){ //iniciando con JQUERY
+  function gameover (valor) {
+    return null;
+  }
+
   $('#validaMayoria').submit(function(event){
     var edad = $('#edad').val();
     console.log(edad);
     if(edad >=18){
-      $('#mnsj1').text('Muy bien puedes pasar');
+      $('#mnsj1').text('Muy bien, que la fuerza te acompañe');
     }else{
-      $('#mnsj1').text('lo siento NO puedes pasar');
+      $('#mnsj1').text('Esta bien pasa, solo no me hare responsable si algo te pasa');
     };
       $('#cadenero article:first-child').fadeOut('slow',function(){
         $('#mnsj1').fadeIn();
       });
     event.preventDefault();
   });
+  
 
 });
